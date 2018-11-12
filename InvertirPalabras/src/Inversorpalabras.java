2635b9b0ab5789c8839294a7358be59c59292199
jmtapia <jmtapia@PROD05OF08.coopjep.fin.ec>
2018-11-12 18:52:30 -0500

    Se retira el Comentario de la clase TODO

--- InvertirPalabras/src/Inversorpalabras.java
+++ InvertirPalabras/src/Inversorpalabras.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Inversorpalabras {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String palabra = "";
 		String palabrainvertida = "";
 		int longitudpalabra = 0;
